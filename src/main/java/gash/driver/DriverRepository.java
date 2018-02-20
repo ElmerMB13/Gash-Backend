84c1427af7f4b312668810a1ddaf23cbc882603f
Alfredo Avila Gonzalez <alfredo1997avila@gmail.com>
2018-02-20 14:34:43 -0600

    Driver repository clean

--- src/main/java/gash/driver/DriverRepository.java
+++ src/main/java/gash/driver/DriverRepository.java
@@ -5,8 +5,4 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DriverRepository extends CrudRepository<Driver, Integer> {
-        void delete(Driver driver);
-        List<Driver> findAll();
-        Driver findOne(Integer id);
-        Driver save(Driver driver);
 }
