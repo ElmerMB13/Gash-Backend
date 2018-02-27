9abc45d0bee5986b313371d9adf0de8d2a4058d1
Brandon Chaves <brandonchaves@Pernixs-Mac-mini.local>
2018-02-27 10:06:46 -0600

    Removing truckrepository

--- src/main/java/gash/truck/TruckRepository.java
+++
@@ -1,9 +0,0 @@
-package gash.truck;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TruckRepository extends CrudRepository<Truck, Integer> {
-    
-}
